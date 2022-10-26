f46399615bef4a8f08c294ebe48723903db5a7ce
Wesley Medeiros <90868789+xwmedx@users.noreply.github.com>
2022-10-26 19:01:56 -0300

    01.04 - Classe Funcionario

--- ByteBank_ADM/Program.cs
+++ ByteBank_ADM/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ByteBank_ADM.Funcionarios;
+
+Funcionario  pedro = new Funcionario();
+
+pedro.Nome = "Pedro";
+pedro.Cpf = "123.456.789-89";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome );
+Console.WriteLine(pedro.GetBonificacao());
