147a90d688c22bae94ce9f0dd71000b02f36f48a
Wesley Medeiros <90868789+xwmedx@users.noreply.github.com>
2022-11-01 18:43:10 -0300

    03.10 - Metodo Abstrato

--- ByteBank_ADM/Funcionarios/Funcionario.cs
+++ ByteBank_ADM/Funcionarios/Funcionario.cs
@@ -6,19 +6,8 @@ using System.Threading.Tasks;
 
 namespace ByteBank_ADM.Funcionarios
 {
-    public class Funcionario
+    public abstract class Funcionario
     {
-        public string Nome { get; set; }
-        public string Cpf { get; private set; }
-        public double Salario { get; protected set; }
-
-        public static int TotalDeFuncionarios { get; private set; }
-
-        public virtual double GetBonificacao()
-        {
-            return this.Salario * 0.10;
-        }
-
         public Funcionario(string cpf, double salario)
         {
             this.Cpf = cpf;
@@ -27,11 +16,14 @@ namespace ByteBank_ADM.Funcionarios
             //Console.WriteLine("Criando um funcionario");
         }
 
-        public virtual void AumentarSalario()
-        {
-            this.Salario *= 1.10;
-        }
+        public string Nome { get; set; }
+        public string Cpf { get; private set; }
+        public double Salario { get; protected set; }
 
+        public static int TotalDeFuncionarios { get; private set; }
 
+        public abstract double GetBonificacao();
+
+        public abstract void AumentarSalario();
     }
 }
